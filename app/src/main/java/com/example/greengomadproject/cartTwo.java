b080ea9576e3262d51ed41ac68c2f4f218d20c60
Dulaksha <dulakshakavi@gmail.com>
2021-09-25 04:17:09 +0530

    created Cart pages

---
+++ app/src/main/java/com/example/greengomadproject/cartTwo.java
@@ -0,0 +1,14 @@
+package com.example.greengomadproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cartTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_two);
+    }
+}
